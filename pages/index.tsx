6811e38c3a7e6bc4c431a581d0871aa70423c55e
devSujeong <dev.sujeong@gmail.com>
2021-10-24 11:52:44 +0900

    add a Header clayout

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,12 +3,15 @@ import type { NextPage } from 'next';
 // import classnames from 'classnames/bind';
 
 // customs
+import Header from '@views/layout/Header.layout';
 
 // const cx = classnames.bind(styles);
 
 const Home: NextPage = () => {
 	return (
-		<div>main</div>
+		<div>
+			<Header />
+		</div>
 	);
 };
 
